7f8ac345a6ba759f89afd43698b8062161a9c154
sopnaparvin1122 <islamanisha860@gmail.com>
2023-09-22 20:23:01 +0600

    basic done

--- basic.js
+++ basic.js
@@ -1,4 +1,17 @@
-let studendName = "sopna"
-studendName = "parvin"
+let firstName = "sopna"
+let lastName = "parvin"
+let fullName = firstName +" "+ lastName
 
-console.log(studendName);
+
+console.log(fullName);
+
+let a = 0.1
+let b = 0.2
+let ab = a + b
+let fixed = ab.toFixed(1)
+
+console.log(parseFloat(fixed));
+
+let accound = 200
+accound += 200
+console.log(accound);
---
+++ conditional.js
@@ -0,0 +1,6 @@
+if ( 10 == 10 ){
+    console.log('welcome');
+}
+else{
+    console.log('Go back');
+}
